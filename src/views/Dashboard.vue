c636160887f76f566c6f64ed3c9e25138b578337
Jamila <jaja_mimi@hotmail.com>
2022-02-08 11:17:45 +0100

    notif

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -26,7 +26,23 @@
                 </v-card>
             </v-col>
         </v-row>
-
+        <v-row >
+            <v-col cols="12" sm="4" offset-sm="2" v-if="messages || messages !== undefined">
+                <v-card dark permanent class="deep-purple accent-4 spacing-playground pa-6">
+                    <h2>Messages</h2>
+                    <p> {{messages.length}} nouveau message</p>
+                    <v-btn @click="checkMessage()">Voir détails</v-btn>
+                </v-card>
+            </v-col>
+            <v-col cols="12" sm="4" offset-sm="1" v-if="taches|| taches !== undefined">
+                <v-card dark permanent class="red accent-4 spacing-playground pa-6">
+                    <h2>Taches</h2>
+                    <p> {{taches.length}} nouvelle tache</p>
+                    <v-btn @click="checkTask()">Voir détails</v-btn>
+                    
+                </v-card>
+            </v-col>
+        </v-row>
     </v-container>
 
 
@@ -39,7 +55,8 @@ import axios from 'axios'
         data() {
             return {
                 url:'http://127.0.0.1:8000/',
-                notifications: null
+                taches: null,
+                messages: null
             }
         },
         computed: {
@@ -56,14 +73,34 @@ import axios from 'axios'
                 Authorization: "Bearer " + this.token
             }
         }).then((response)=> {
-            if (response.data.valid == true) {
-                this.notifications = response.data.data
-            } else {
-                this.notifications = null
-            }
+                this.taches = response.data.taches
+                this.messages = response.data.messages
         })
-        
-        }
+        },
+        methods: {
+            checkMessage() {
+                axios.put('http://127.0.0.1:8000/api/notifications/messages',{}, {
+                headers: {
+                Authorization: "Bearer " + this.token
+                }
+                }).then((response)=>{
+                    console.log(response.data);
+                    this.messages= null
+                })
+                this.$router.push({name:'Messages'});
+            },
+            checkTask() {
+                axios.put('http://127.0.0.1:8000/api/notifications/taches',{}, {
+                headers: {
+                Authorization: "Bearer " + this.token
+                }
+                }).then((response)=>{
+                    console.log(response.data);
+                    this.messages= null
+                })
+                this.$router.push({name:'Taches'});
+            }
+        },
     }
 </script>
 
